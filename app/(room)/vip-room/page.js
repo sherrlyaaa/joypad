e115464d653d49b42dc25314b5fc3d7c2fd0d50c
Sherly Angelina <sherlyangelina0715@gmail.com>
2024-12-24 23:19:58 +0700

    menambahkan findroom, regularroom, viproom, vvip room, modifikasi yang perlu di modifikasi

--- app/(room)/vip-room/page.js
+++ app/(room)/vip-room/page.js
@@ -4,9 +4,9 @@ import { useState } from 'react';
 import Header from "../../header";
 import Footer from "../../footer";
 import Link from 'next/link';
-import "../../../styles/regular.css";
+import "../../../styles/vip.css";
 
-const RegularRoom = () => {
+const VipRoom = () => {
     const [activeSection, setActiveSection] = useState('facilities');
 
     const handleNavClick = (section, e) => {
@@ -19,48 +19,71 @@ const RegularRoom = () => {
             return (
                 <div className="features-container">
                     <div>
-                        <img src="/image/produk-ps-5.png" alt="Playstation 5"/>
-                        <h3>Playstation 5<br/>(2 Controllers)</h3>
+                    <img 
+                        style={{ marginTop: "40px" }} 
+                        src="/image/produk-ps-5.png" 
+                        alt="Playstation 5" 
+                        />
+                        <h3>
+                        Playstation 5<br />
+                        (2 Controllers)
+                        </h3>
                     </div>
                     <div>
-                        <img src="/image/smart-tv.png" alt="4K Smart TV"/>
-                        <h3>4K Smart TV<br/>55 inch</h3>
+                        <img style={{ marginTop: "40px" }} src="/image/smart-tv.png" alt="4K Smart TV" />
+                        <h5>4K Smart TV 55 inch</h5>
                     </div>
                     <div>
-                        <img src="/image/cctv.png" alt="CCTV"/>
-                        <h3>CCTV</h3>
+                        <img style={{ marginBottom: "40px" }} src="/image/cctv.png" alt="CCTV" />
+                        <h4>CCTV</h4>
                     </div>
                     <div>
-                        <img src="/image/charge.png" alt="Charging Station"/>
+                        <img
+                        style={{ width: "152px", height: "152px" }}
+                        src="/image/charge.png"
+                        alt="Charging Station"
+                        />
                         <h3>Charging Station</h3>
                     </div>
                     <div>
-                        <img src="/image/schedule.png" alt="Booking Schedule"/>
+                        <img src="/image/schedule.png" alt="Booking Schedule" />
                         <h3>Booking Schedule</h3>
                     </div>
                     <div>
-                        <img src="/image/wifi.png" alt="Wifi 300 mbps"/>
+                        <img src="/image/wifi.png" alt="Wifi 300 mbps" />
                         <h3>Wifi 300 mbps</h3>
                     </div>
                     <div>
-                        <img src="/image/room.png" alt="Private Room"/>
+                        <img src="/image/room.png" alt="Private Room" />
                         <h3>Private Room</h3>
                     </div>
                     <div>
-                        <img src="/image/ac.png" alt="Air Conditioner"/>
+                        <img src="/image/ac.png" alt="Air Conditioner" />
                         <h3>Air Conditioner</h3>
                     </div>
                     <div>
-                        <img src="/image/psplus.png" alt="psplus"/>
-                        <h3>PS Plus</h3>
+                        <img
+                        style={{ width: "150px", height: "150px" }}
+                        src="/image/console.png"
+                        alt="Nintendo Switch"
+                        />
+                        <h3>
+                        Nintendo Switch <br />
+                        (2 Controllers)
+                        </h3>
                     </div>
                     <div>
-                        <img src="/image/netflix.png" alt="netflix"/>
-                        <h3>Netflix</h3>
+                        <img
+                        style={{ verticalAlign: "top" }}
+                        src="/image/psplus.png"
+                        alt="PS Plus"
+                        />
                     </div>
                     <div>
-                        <img src="/image/youtube.png" alt="youtube"/>
-                        <h3>YouTube</h3>
+                        <img src="/image/netflix.png" alt="Netflix" />
+                    </div>
+                    <div>
+                        <img src="/image/youtube.png" alt="YouTube" />
                     </div>
                 </div>
             );
@@ -68,19 +91,19 @@ const RegularRoom = () => {
             return (
                 <div className="games-container">
                     <div>
-                        <img src="/image/exgame/astrobot.png" alt="Astro Bot"/>
-                        <h3>Astro Bot</h3>
-                    </div>
-                    <div>
-                        <img src="/image/exgame/wukong.png" alt="Black Myth Wukong"/>
-                        <h3>Black Myth Wukong</h3>
-                    </div>
-                    <div>
-                        <img src="/image/exgame/uncharted.png" alt="Uncharted : Legacy of Thieves Collection"/>
+                        <img src="/image/exgame/spider.png" alt="Marvel’s Spider-Man: Miles Moarales"/>
                         <h3>
-                        Uncharted : Legacy of Thiev-<br />
-                        es Collection
+                            Marvel’s Spider-Man: Miles-<br />
+                            Moarales
                         </h3>
+                    </div>
+                    <div>
+                        <img src="/image/exgame/fortnite.png" alt="Fortnite"/>
+                        <h3>Fortnite</h3>
+                    </div>
+                    <div>
+                        <img src="/image/exgame/ragnarok.png" alt="God of War Ragnarok"/>
+                        <h3>God of War Ragnarok</h3>
                     </div>
                 </div>    
             );
@@ -92,12 +115,12 @@ const RegularRoom = () => {
             <Header/>
             <div>
                 <div className="image-container">
-                    <img src="/image/regular.png" alt="Regular" />
+                    <img src="/image/vip.png" alt="Vip" />
                 </div>
                 <div className="type">
                     <div className="room-info">
-                        <h1 className="room-title">REGULAR ROOM</h1>
-                        <p className="room-price">Rp. 20.000/hour</p>
+                        <h1 className="room-title">VIP ROOM</h1>
+                        <p className="room-price">Rp. 35.000/hour</p>
                     </div>
                     <div>
                         <button className="book-btn">BOOK</button>
@@ -136,4 +159,11 @@ const RegularRoom = () => {
     );
 };
 
-export default RegularRoom;
+export default VipRoom;
+
+
+
+
+
+
+
